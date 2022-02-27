90ad0bdd0822751d9a203e015fd6b46232d88cd7
kamilsamaj <kamilsamaj@gmail.com>
2022-02-27 22:17:33 +0100

    fully parameterize version, more info

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 [The amazing Kitty terminal](https://github.com/kovidgoyal/kitty/releases) doesn't provide
 Debian packages - this is a way to create it.
 
+# Prerequisites
+* Navigate to the [Kitty GitHub Releases](https://github.com/kovidgoyal/kitty/releases) and find the version you want.
+* Update the [create_pkg.sh](./create_pkg.sh) `VERSION` variable
+
 # Run
 ```shell
 ./create_pkg.sh
+```
+
+# Install the package
+```
+sudo dpkg -i kitty*.deb
+sudo apt install -f  # in case some dependency packages were not installed
 ```
--- create_pkg.sh
+++ create_pkg.sh
@@ -1,14 +1,21 @@
 #!/bin/bash -x
-VERSION="0.24.2"
+VERSION="0.24.2"  # CHANGE to update
+
+
 TMP_DIR=$(mktemp -d)
-trap "rm -rf $TMP_DIR" EXIT
+trap 'rm -rf $TMP_DIR' EXIT
 
 curl -sL "https://github.com/kovidgoyal/kitty/releases/download/v${VERSION}/kitty-${VERSION}-x86_64.txz" -O
 
-kitty-0.24.2-x86_64.txz
-tar xvf kitty-0.24.2-x86_64.txz
+tar xvf kitty-${VERSION}-x86_64.txz
 
 mkdir -p "${TMP_DIR}/usr"
 mv {bin,lib,share} "$TMP_DIR/usr"
 cp -avr DEBIAN "$TMP_DIR"
-dpkg-deb -b "$TMP_DIR" kitty-${VERSION}_amd64.deb
+sed -i "s/__VERSION__/${VERSION}/g" "${TMP_DIR}/DEBIAN/control"
+dpkg-deb -b "$TMP_DIR" kitty-${VERSION}-1_amd64.deb
+
+set +x
+echo
+echo "kitty-${VERSION}-1_amd64.deb created in the current directory. You can install it with:"
+echo "sudo dpkg -i kitty-${VERSION}-1_amd64.deb"
